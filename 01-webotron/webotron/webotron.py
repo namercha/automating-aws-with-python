7fdfc2ffa43a8d90664b48440c31985d672b1bbb
Nabil Merchant <nabil@Nabils-MacBook-Pro.local>
2019-05-03 14:01:04 -0400

    Create and setup new bucket.

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+from botocore.exceptions import ClientError
 
 session = boto3.Session()
 s3 = session.resource('s3')
@@ -22,5 +23,51 @@ def list_bucket_objects(bucket):
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
 
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and configure S3 bucket."
+    s3_bucket = None
+    try:
+        if session.region_name =='us-east-1':
+            s3_bucket = s3.create_bucket(Bucket='nabil.pro',)
+        else:
+            s3_bucket = s3.create_bucket(
+            Bucket='nabil.pro',
+            CreateBucketConfiguration={'LocationConstraint': session.region_name})
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            s3_bucket = s3.Bucket(bucket)
+        else:
+            raise e
+
+
+    policy = """{
+        "Version":"2012-10-17",
+        "Statement":[{
+	     "Sid":"PublicReadGetObject",
+         "Effect":"Allow",
+	        "Principal": "*",
+                "Action":["s3:GetObject"],
+                "Resource":["arn:aws:s3:::%s/*"
+                ]
+            }
+        ]
+    }
+    """ % s3_bucket.name
+    policy = policy.strip()
+
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration={
+    'ErrorDocument': {'Key':'error.html'},
+    'IndexDocument': {'Suffix':'index.html'}
+    })
+
+    return
+
 if __name__ == '__main__':
+    print("You are operating in: " + session.region_name)
     cli()
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Repository for the A Cloud Guru Course *Automating AWS with Python*
 
 ## 01-webotron
-Webotron is a script that will sync a local directory to an s3 bucket, and optionally configure Route53 and CloudFront as well. 
+Webotron is a script that will sync a local directory to an s3 bucket, and optionally configure Route53 and CloudFront as well.
 
 ### Features
 
@@ -10,3 +10,4 @@ Webotron currently has the following features:
 
 - List buckets
 - List the contents of a bucket
+- Create and set up bucket
